e612e2fbfd31d543424c7296589892888f86515f
ferdeolazabal <ferdeolazabal@gmail.com>
2024-01-02 21:29:29 -0300

    Add to serve production client in server
    - Add public to gitignore

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -12,7 +13,7 @@ app.use(
 );
 
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
 
 module.exports = app;
