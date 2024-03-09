242f918dfaa3df684cad2baba8fe50b7ad734a49
ferdeolazabal <ferdeolazabal@gmail.com>
2024-03-09 20:53:52 -0300

    Add launches model
    - Add controller & router

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("January 27, 2027"),
+  destination: "Kepler-422 b",
+  customer: ["NASA", "CAVS"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+  launches,
+};
