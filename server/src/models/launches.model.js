c7457a97be0eb0531e8c008f7bd62fba9abe5ea3
ferdeolazabal <ferdeolazabal@gmail.com>
2024-03-09 23:31:57 -0300

    Add new launch functionality to model & controller

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("January 27, 2027"),
-  destination: "Kepler-422 b",
+  target: "Kepler-422 b",
   customer: ["NASA", "CAVS"],
   upcoming: true,
   success: true,
@@ -17,6 +19,20 @@ const getAllLaunches = () => {
   return Array.from(launches.values());
 };
 
+const addNewLaunch = (launch) => {
+  latestFlightNumber++;
+  launches.set(
+    launch.flightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customer: ["NASA", "CAVS"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+};
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
