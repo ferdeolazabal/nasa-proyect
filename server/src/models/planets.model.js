90b11995c3a8ecb3c36710b3c63f203259bdc9fc
ferdeolazabal <ferdeolazabal@gmail.com>
2024-06-21 14:52:10 -0300

    Add and apply planets mongo model
    - Add Mongo schema for planets entity
    - Update planets model logic with planets mongo schema
    - Update controller
    - Update front file where planet name prop is called as name was updated for camelcase string

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { parse } = require("csv-parse");
 
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -19,21 +19,39 @@ const loadPlanetsData = () => {
       path.join(__dirname, "..", "..", "data", "kepler_data.csv")
     )
       .pipe(parse({ comment: "#", columns: true }))
-      .on("data", (data) => {
-        if (isHabitablePlanet(data)) habitablePlanets.push(data);
+      .on("data", async (data) => {
+        if (isHabitablePlanet(data)) {
+          await savePlanets(data);
+        }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log("Number of habitable planets: ", habitablePlanets.length);
-        resolve(habitablePlanets);
+      .on("end", async () => {
+        const countPlanetsFound = await planets.countDocuments();
+        console.log("Number of habitable planets: ", countPlanetsFound);
+        resolve(planets);
       });
   });
+};
+const getAllPlanets = async () => {
+  return await planets.find({});
+};
+
+const savePlanets = async (planet) => {
+  try {
+    await planets.updateOne(
+      { keplerName: planet.kepler_name },
+      { keplerName: planet.kepler_name },
+      { upsert: true }
+    );
+  } catch (e) {
+    console.error(`Could not save planet ${planet.kepler_name}`, { e });
+  }
 };
 
 module.exports = {
   loadPlanetsData,
-  getAllPlanets: () => habitablePlanets,
+  getAllPlanets,
 };
