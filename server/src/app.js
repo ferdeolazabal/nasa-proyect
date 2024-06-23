522a208d7462e988acaf79b30bd1d21ea4c833c3
ferdeolazabal <ferdeolazabal@gmail.com>
2024-06-23 12:06:39 -0300

    Correct format code

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,7 @@ const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
